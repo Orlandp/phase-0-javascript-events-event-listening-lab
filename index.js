ead25b3620a7a9704cec56d192e9a21f5b2c4112
pknowledge <jamesblssings@gmail.com>
2024-10-08 22:52:03 +0300

    placing if in functions

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 function addingEventListener() {
+    function clickAlert() {
+        alert('I was clicked!');
+    }
+    
+    function addingEventListener() {
+        const input = document.getElementById('button');
+        
+        if (input) {
+            input.addEventListener('click', clickAlert);
+        }
+    }
+    
+    addingEventListener();
+    
 }
